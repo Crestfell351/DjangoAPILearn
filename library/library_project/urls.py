a48024c0b1c7d502342f9287d2c2678b60507898
Aphiwe Makhathini <vmelamore@gmail.com>
2024-10-24 12:51:19 +0200

    fixed book.views

--- library/library_project/urls.py
+++ library/library_project/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
-from .views import BookListView
+from books.views import BookListView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
